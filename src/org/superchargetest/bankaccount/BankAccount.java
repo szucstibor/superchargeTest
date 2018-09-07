fc33008102dcfd3c16bb67fe6b14777314722746
Szűcs Tibor <job.szucstibor@gmail.com>
2018-09-07 14:14:34 +0200

    Added print transactions method with optional filtering

--- src/org/superchargetest/bankaccount/BankAccount.java
+++ src/org/superchargetest/bankaccount/BankAccount.java
@@ -1,6 +1,7 @@
 package org.superchargetest.bankaccount;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BankAccount {
@@ -9,7 +10,7 @@ public class BankAccount {
 
     private float balance;
 
-    private List<String[]> transactionHistory;
+    private List<String[]> transactionHistory = new ArrayList<>();
 
     public BankAccount(int balance) {
         this.balance = balance;
@@ -17,6 +18,10 @@ public class BankAccount {
 
     public Long getID() {
         return ID;
+    }
+
+    public void setID(Long ID){
+        this.ID = ID;
     }
 
     public float getBalance() {
@@ -38,6 +43,8 @@ public class BankAccount {
     public void withdrawal(int amount){
         if (amount > this.balance){
             throw new IllegalArgumentException("Your balance is lower than the requested amount.\nYour current balnce is: " + this.balance);
+        } else if (amount <= 0){
+            throw new IllegalArgumentException("Please enter a positive number, greater than 0 for withdrawal.");
         }
         this.balance -= amount;
         System.out.println("You have withdrawed " + amount + ".\nYour new balance is: " + this.balance);
@@ -45,7 +52,7 @@ public class BankAccount {
     }
 
     public void deposit(int amount){
-        if (amount < 0){
+        if (amount < 1){
             throw new IllegalArgumentException("Deposit has to be greater than 0");
         }
         this.balance += amount;
@@ -56,5 +63,27 @@ public class BankAccount {
     private void addToTransactionHistory(String type, int amount) {
         String[] withdrawalHistory = {type, LocalDateTime.now().toString(), String.valueOf(amount), String.valueOf(this.balance)};
         this.transactionHistory.add(withdrawalHistory);
+    }
+
+    public void printTransacrions(){
+        System.out.println("Type, Date, Amount, Balance");
+        for (String[] history: transactionHistory) {
+            for (String field: history) {
+                System.out.print(field + ", ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void printTransacrions(String type){
+        System.out.println("Type, Date, Amount, Balance");
+        for (String[] history: transactionHistory) {
+            if (history[0].equals(type)){
+                for (String field: history) {
+                    System.out.print(field + ", ");
+                }
+                System.out.println();
+            }
+        }
     }
 }
